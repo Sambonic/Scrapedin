af5ef81c9c228d68f3512d23e3f09d716cb10801
Sambonic <samehaymancs@gmail.com>
2025-01-31 01:27:28 +0200

    Quick Fixes
    - Fixed miswritten csv file header

--- src/managers/csv_handler.py
+++ src/managers/csv_handler.py
@@ -1,11 +1,10 @@
-from src.config.common_imports import csv, datetime, os
+from src.config.common_imports import csv, os
 from src.config.path_config import path_manager
 from src.managers.logger_manager import logger
 
 class CSVHandler:
     def __init__(self, role):
         path_manager.create_csv_file(role=role)
-
 
     def write_header(self, fieldnames):
         """
@@ -18,8 +17,7 @@ class CSVHandler:
             writer.writeheader()
             logger.info(f"Header written to {file_path}")
 
-
-    def _add_to_csv(self, job_details:dict) -> None:
+    def _add_to_csv(self, job_details: dict) -> None:
         """
         Adds job listing details to a CSV file. Creates necessary folders if needed, 
         formats the data, and appends it to an appropriately named CSV file.
@@ -30,13 +28,13 @@ class CSVHandler:
         with open(file_path, mode='a', newline='', encoding="utf-8") as file:
             writer = csv.DictWriter(file, fieldnames=job_details.keys())
 
-            # Write header only if file is  empty
-            if not is_file_empty:
+            # Write header only if the file is empty
+            if is_file_empty:
                 try:
                     logger.info("Creating a new CSV file with header...")
                     writer.writeheader()
                 except OSError as e:
                     logger.error(f"An error occurred while creating the file: {str(e)}")
-            
+
             writer.writerow(job_details)
             logger.info("Job details added to CSV file successfully.")
