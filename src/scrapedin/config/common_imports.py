29a331fbd144f03f9b2145bd6b9268d9312c772f
sambonic <samehaymancs@gmail.com>
2025-09-06 19:43:58 +0300

    Minor Adjustments Modified pyproject.toml to include project dependencies Removed legacy imports Deleted requirements.txt to avoid redundant maintenance alongside pyproject.toml Modified path_config.py to rely on finding pyproject.toml for pathing Modified main.yml workflow to properly install the package alongside its dependencies in Linux VM

--- src/scrapedin/config/common_imports.py
+++ src/scrapedin/config/common_imports.py
@@ -7,6 +7,8 @@ import time
 from datetime import datetime
 from typing import Any, Dict, List, Optional, Tuple
 from pathlib import Path
+import regex as re
+
 
 # Logging and Debugging
 import logging
@@ -16,19 +18,11 @@ from logging import DEBUG, FileHandler, Formatter, INFO, Logger, StreamHandler
 # Performance Analysis and Benchmarking
 import psutil
 
-# Data Preprocessing, Cleaning, and Analyzing
-import numpy as np
-import pandas as pd
-import regex as re
-
 # Web Scraping and Automation
 import requests
 from selenium import webdriver
-from selenium.webdriver import Firefox, Chrome, ChromeOptions
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver import Firefox
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.common.exceptions import TimeoutException
